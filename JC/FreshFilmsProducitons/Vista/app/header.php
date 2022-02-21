22749fe4b28a01451764a9cd8778972a9fd74e11
SergioWork-075 <sergy0754@gmail.com>
2022-02-22 00:00:51 +0100

    Este es el producto de neustro trabjao AMIGO

--- JC/FreshFilmsProducitons/Vista/app/header.php
+++ JC/FreshFilmsProducitons/Vista/app/header.php
@@ -1,4 +1,4 @@
-<?php  $SESSION['web']="../FreshFilmsProducitons";  ?>
+<?php  $ruta = "http://35.181.8.108/JC/FreshFilmsProducitons"; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,7 +25,7 @@
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="../index.php">Fresh Films Productions</a>
+        <a class="navbar-brand" href="<?php echo $ruta."/index.php"?>">Fresh Films Productions</a>
         <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
@@ -35,7 +35,7 @@
                 <li  class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Portfolio</a></li>
                 <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">About</a></li>
                 <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contact</a></li>
-                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="/Vista/formulariozuri.php">Registrase</a></li>
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="<?php echo $ruta."/Vista/formulariozuri.php"?>">Registrase</a></li>
             </ul>
         </div>
     </div>
