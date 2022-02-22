8755938c57f82025a3404f3168625854960c9935
ZuRi-To <91980222+ZuRi-To@users.noreply.github.com>
2022-02-22 20:28:21 +0100

    video arreglao

--- JC/FreshFilmsProducitons/Vista/video.php
+++ JC/FreshFilmsProducitons/Vista/video.php
@@ -3,8 +3,11 @@ require('app/header.php');
 ?>
 <!-- Masthead-->
 <header class="masthead bg-primary text-white text-center">
-            <iframe class="embed-responsive embed-responsive-16by9"  width="650px" height="450px" src="https://www.youtube.com/embed/QaKbB1PzXpA" title="YouTube video player"
-                    allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<div class="container">
+    <div class="ratio ratio-16x9">
+        <iframe src="https://www.youtube.com/embed/kvhUNv59XdY" title="YouTube video" allowfullscreen></iframe>
+    </div>
+</div>
 </header
 <?php
 require('app/footer.php');
