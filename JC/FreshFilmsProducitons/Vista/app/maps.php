e88b53231ba3327d4dd1dddc9ad9eeb383490c2c
SergioWork-075 <sergy0754@gmail.com>
2022-02-22 19:28:14 +0100

    Feos modos

---
+++ JC/FreshFilmsProducitons/Vista/app/maps.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        #map {
+            height: 500px;
+            width: 100%;
+        }
+    </style>
+    <script>function iniciarMap(){
+            var coord = {lat:40.4538891836561 ,lng: -3.7185732846581763};
+            var map = new google.maps.Map(document.getElementById('map'),{
+                zoom: 15,
+                center: coord
+            });
+            var marker = new google.maps.Marker({
+                position: coord,
+                map: map
+            });
+        }</script>
+</head>
+<body>
+    <div id="map"></div>
+    <script src=""></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDohJJmi37KRYta7lXYchfd2DA7l2CcZ8s&callback=iniciarMap"></script>
+</body>
+</html>
