2d09e1d0302fc46f489ed626a7e39d7e8b4b9f9f
ZuRi-To <91980222+ZuRi-To@users.noreply.github.com>
2022-02-23 22:32:20 +0100

    archivos que faltaban de bootstrap

--- JC/JC/Vista/app/header.php
+++ JC/JC/Vista/app/header.php
@@ -6,32 +6,51 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>After Effects Films</title>
+
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
     <LINK REL=StyleSheet HREF="css/estilo.css" TYPE="text/css" MEDIA=screen>
     <LINK REL=StyleSheet HREF="../css/estilo.css" TYPE="text/css" MEDIA=screen>
     <link rel="stylesheet" href="../../css/bootstrap-5.0.2-dist/css/bootstrap.css" >
+
+    <!--afecta al nav-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
 <!-- Navigaion-->
-<nav class="navegacion navbar navbar-dark bg-dark navbar-expand-lg bg-secondary text-uppercase fixed-top">
-    <div class="container">
-        <a class="navbar-brand" href="<?php echo $ruta?>">Fresh Films Productions</a>
-        <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            Menu
-            <i class="fas fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ms-auto">
-                <li  class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Portfolio</a></li>
-                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">About</a></li>
-                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contact</a></li>
-                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="<?php echo $ruta."formulario.php"?>">Registrase</a></li>
-            </ul>
-        </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Fresh Films Productions</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+
+            <li class="nav-item">
+                <a class="nav-link" href="#portfolio">Video</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#about"></a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#contact">Sobre nosotros</a>
+            </li>
+
+            <li class="nav-item ">
+                <a class="nav-link" href=""<?php echo $ruta."Vista/formulariozuri.php"?>>Registrarse</a>
+            </li>
+
+        </ul>
+
     </div>
 </nav>
+
 <!-- Navigation-->
